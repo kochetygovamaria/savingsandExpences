385379347b9ec8f51d3da3b0b5d78699938bf15b
MashaK <kochetygova.m@husky.meu.edu>
2018-02-10 13:05:24 -0500

    first commit

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
+    $scope.savings1="236";
+    $scope.savings2="123";
+    $scope.savings3="236";
+    $scope.savings4="123";
+    $scope.savingsDescription1="House";
+    $scope.savingsDescription2="Trip to Argentina";
+    $scope.savingsDescription3="Iphone 8Plus";
+    $scope.savingsDescription4="New honda Civic";
 });
