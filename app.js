60e96045980d16b92687a83be976d70ab9ea5de5
MashaK <kochetygova.m@husky.meu.edu>
2018-02-10 16:24:16 -0500

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['chart.js']);
 app.controller('myCtrl', function($scope) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
@@ -10,4 +10,34 @@ app.controller('myCtrl', function($scope) {
     $scope.savingsDescription2="Trip to Argentina";
     $scope.savingsDescription3="Iphone 8Plus";
     $scope.savingsDescription4="New honda Civic";
+
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.options = {
+      scales: {
+        yAxes: [
+          {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          },
+          {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+          }
+        ]
+      }
+    };
 });
